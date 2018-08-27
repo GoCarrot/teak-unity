d7d7838a0e7696a468236789849897a0ec88c902
Pat Wilson <zerostride@gmail.com>
2018-08-27 14:37:24 -0700

    Added new parameters for pluginPurchaseFailed

--- Assets/Teak/Teak.cs
+++ Assets/Teak/Teak.cs
@@ -348,7 +348,7 @@ public partial class Teak : MonoBehaviour
     private void Prime31PurchaseFailed(string error, int errorCode)
     {
         AndroidJavaClass teak = new AndroidJavaClass("io.teak.sdk.Teak");
-        teak.CallStatic("pluginPurchaseFailed", errorCode);
+        teak.CallStatic("pluginPurchaseFailed", errorCode, "prime31");
     }
 
     private void OpenIABPurchaseSucceded<T>(T purchase)
@@ -361,7 +361,7 @@ public partial class Teak : MonoBehaviour
     private void OpenIABPurchaseFailed(int errorCode, string error)
     {
         AndroidJavaClass teak = new AndroidJavaClass("io.teak.sdk.Teak");
-        teak.CallStatic("pluginPurchaseFailed", errorCode);
+        teak.CallStatic("pluginPurchaseFailed", errorCode, "openiab");
     }
 
 #elif UNITY_IPHONE || UNITY_WEBGL
