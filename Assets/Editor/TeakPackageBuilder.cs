85cb8234b3d2b53db25a29d029e18c6bb5c7c4cd
Pat Wilson <zerostride@gmail.com>
2018-11-26 12:34:19 -0800

    No longer including these

--- Assets/Editor/TeakPackageBuilder.cs
+++ Assets/Editor/TeakPackageBuilder.cs
@@ -34,9 +34,7 @@ public class TeakPackageBuilder : Editor
     public static void BuildUnityPackage()
     {
         string[] assetPaths = new string[] {
-            "Assets/Teak",
-            "Assets/Plugins/WebGL/Teak.jslib",
-            "Assets/Plugins/Android/teak.aar"
+            "Assets/Teak"
         };
         AssetDatabase.ExportPackage(assetPaths, "Teak.unitypackage", ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies);
     }
