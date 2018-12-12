9e155a200e6bdb71851d95e8fe0065d7b1185642
Pat Wilson <zerostride@gmail.com>
2018-12-12 10:58:38 -0800

    Changelist!

---
+++ docrev.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+sections = ['Breaking Changes', 'New Features', 'Bug Fixes']
+
+lines = []
+major, minor, patch = nil
+ARGF.each do |line|
+  if (match = line.match(/^(\d+\.)?(\d+\.)?(\*|\d+)$/))
+    major, minor, patch = match.captures.map(&:to_i)
+  elsif sections.include? line.strip
+    lines << ":#{line.strip}:"
+  elsif line.start_with? 'Native SDK Versions'
+    break
+  else
+    lines << "    * #{line.strip}" unless line.strip.empty?
+  end
+end
+
+if major.nil? && minor.nil? && patch.nil?
+  STDERR.puts 'Could not find version number in input.'
+  return
+end
+
+File.open("docs/changelog/#{major}.#{minor}.#{patch}.rst", 'w') do |file|
+  version_str = "#{major}.#{minor}.#{patch}"
+  file.write <<~TEMPLATE
+    .. include:: ../changelog_entry.rst
+
+    #{version_str}
+    #{'=' * version_str.length}
+    #{lines.join("\n")}
+  TEMPLATE
+end
