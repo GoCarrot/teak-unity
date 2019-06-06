f26c15ec14a8493e86348e6646aa7ba52fa47fc9
Pat Wilson <pat@teak.io>
2019-06-06 14:46:57 -0700

    Better handling of adding entitlements to iOS projects

--- Assets/Teak/Editor/TeakPostProcessBuild.cs
+++ Assets/Teak/Editor/TeakPostProcessBuild.cs
@@ -42,33 +42,6 @@ public class TeakPostProcessBuild {
         File.WriteAllText(plistPath, AddTeakEntriesToPlist(File.ReadAllText(plistPath)));
 
         /////
-        // Make sure push-notifications are enabled
-        project.AddCapability(unityTarget, PBXCapabilityType.PushNotifications);
-
-        // Unity 2018 TODO:
-        //      project.GetBuildPropertyForAnyConfig(unityTarget, "CODE_SIGN_ENTITLEMENTS")
-        // Try and get the name of the entitlements file, if it exists.
-
-        /////
-        // Add/modify entitlements
-        string entitlementsFileName = "Unity-iPhone.entitlements";
-        string entitlementsPath = pathToBuiltProject + "/Unity-iPhone/" + entitlementsFileName;
-
-        // If the entitlements file doesn't exist, create a blank one
-        if (!File.Exists(entitlementsPath)) {
-            PlistDocument plist = new PlistDocument();
-            plist.Create();
-            File.WriteAllText(entitlementsPath, plist.WriteToString());
-
-            // Add to Xcode Project
-            project.AddFile(entitlementsPath, entitlementsFileName);
-            project.AddBuildProperty(unityTarget, "CODE_SIGN_ENTITLEMENTS", entitlementsPath);
-        }
-
-        // Add entitlements
-        File.WriteAllText(entitlementsPath, AddTeakEntitlements(File.ReadAllText(entitlementsPath)));
-
-        /////
         // Add Teak app extensions
         string[] teakExtensionCommonFrameworks = new string[] {"AdSupport", "AVFoundation", "CoreGraphics", "ImageIO", "MobileCoreServices", "StoreKit", "SystemConfiguration", "UIKit", "UserNotifications"};
 
@@ -83,19 +56,13 @@ public class TeakPostProcessBuild {
         /////
         // Write out modified project
         project.WriteToFile(projectPath);
-    }
 
-    private static string AddTeakEntitlements(string inputPlist) {
-        PlistDocument plist = new PlistDocument();
-        plist.ReadFromString(inputPlist);
-
-        // Add aps-environment
-        plist.root.SetString("aps-environment", UnityEngine.Debug.isDebugBuild ? "development" : "production");
-
-        // Add associated domains
-        AddElementToArrayIfMissing(plist, "com.apple.developer.associated-domains", "applinks:" + TeakSettings.ShortlinkDomain);
-
-        return plist.WriteToString();
+        /////
+        // Add/modify entitlements
+        ProjectCapabilityManager capabilityManager = new ProjectCapabilityManager(projectPath, pathToBuiltProject + "/dev.entitlements", PBXProject.GetUnityTargetName());
+        capabilityManager.AddPushNotifications(UnityEngine.Debug.isDebugBuild);
+        capabilityManager.AddAssociatedDomains(new string[] {"applinks:" + TeakSettings.ShortlinkDomain});
+        capabilityManager.WriteToFile();
     }
 
     private static string AddTeakEntriesToPlist(string inputPlist) {
