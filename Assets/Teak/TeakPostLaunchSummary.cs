aa7a2c7d8a4a8767a4c58129532fa3625de1c616
Pat Wilson <pat@teak.io>
2022-03-15 15:50:39 -0700

    More docs

--- Assets/Teak/TeakPostLaunchSummary.cs
+++ Assets/Teak/TeakPostLaunchSummary.cs
@@ -2,27 +2,34 @@ using System;
 using System.Collections.Generic;
 
 public class TeakPostLaunchSummary {
+    /// <summary>The link used to launch the game, or ``null``.</summary>
     public string LaunchLink { get; private set; }
-    public string ScheduleName { get; private set; }
-    public string ScheduleId { get; private set; }
-    public string CreativeName { get; private set; }
-    public string CreativeId { get; private set; }
-    public string RewardId { get; private set; }
-    public string ChannelName { get; private set; }
-    public string DeepLink { get; private set; }
-    public string SourceSendId { get; private set; }
 
-    public TeakPostLaunchSummary(Dictionary<string, object> json) {
-        this.LaunchLink = json.ContainsKey("launch_link") ? json["launch_link"] as string : null;
-        this.ScheduleName = json.ContainsKey("teakScheduleName") ? json["teakScheduleName"] as string : null;
-        this.ScheduleId = json.ContainsKey("teakScheduleId") ? json["teakScheduleId"] as string : null;
-        this.CreativeName = json.ContainsKey("teakCreativeName") ? json["teakCreativeName"] as string : null;
-        this.CreativeId = json.ContainsKey("teakCreativeId") ? json["teakCreativeId"] as string : null;
-        this.RewardId = json.ContainsKey("teakRewardId") ? json["teakRewardId"] as string : null;
-        this.ChannelName = json.ContainsKey("teakChannelName") ? json["teakChannelName"] as string : null;
-        this.DeepLink = json.ContainsKey("teakDeepLink") ? json["teakDeepLink"] as string : null;
-        this.SourceSendId = json.ContainsKey("teakNotifId") ? json["teakNotifId"] as string : null;
-    }
+    /// <summary>The name of the schedule for the notification on the Teak Dashboard, or ``null`` if it was not a scheduled notification.</summary>
+    public string ScheduleName { get; private set; }
+
+    /// <summary>The id of the schedule in the Teak CMS, or ``null`` if it was not a scheduled notification.</summary>
+    public string ScheduleId { get; private set; }
+
+    /// <summary>The name of the link or notification on the Teak Dashboard.</summary>
+    public string CreativeName { get; private set; }
+
+    /// <summary>The id of the link or notification in the Teak CMS.</summary>
+    public string CreativeId { get; private set; }
+
+    /// <summary>Opaque reward identifier, or ``null`` if no reward.</summary>
+    public string RewardId { get; private set; }
+
+    /// <summary>
+    /// The name of the Teak 'channel', one of: ``ios_push``, ``android_push``, ``fb_a2u``, ``email``, ``generic_link``.
+    /// </summary>
+    public string ChannelName { get; private set; }
+
+    /// <summary>The deep link used to launch the game, or ``null``.</summary>
+    public string DeepLink { get; private set; }
+
+    /// <summary>Opaque notification or email identifyer, or ``null``.</summary>
+    public string SourceSendId { get; private set; }
 
     /// <summary>
     /// Returns a string that represents the current object.
@@ -42,4 +49,18 @@ public class TeakPostLaunchSummary {
                              this.SourceSendId
                             );
     }
+
+    /// @cond hide_from_doxygen
+    public TeakPostLaunchSummary(Dictionary<string, object> json) {
+        this.LaunchLink = json.ContainsKey("launch_link") ? json["launch_link"] as string : null;
+        this.ScheduleName = json.ContainsKey("teakScheduleName") ? json["teakScheduleName"] as string : null;
+        this.ScheduleId = json.ContainsKey("teakScheduleId") ? json["teakScheduleId"] as string : null;
+        this.CreativeName = json.ContainsKey("teakCreativeName") ? json["teakCreativeName"] as string : null;
+        this.CreativeId = json.ContainsKey("teakCreativeId") ? json["teakCreativeId"] as string : null;
+        this.RewardId = json.ContainsKey("teakRewardId") ? json["teakRewardId"] as string : null;
+        this.ChannelName = json.ContainsKey("teakChannelName") ? json["teakChannelName"] as string : null;
+        this.DeepLink = json.ContainsKey("teakDeepLink") ? json["teakDeepLink"] as string : null;
+        this.SourceSendId = json.ContainsKey("teakNotifId") ? json["teakNotifId"] as string : null;
+    }
+    /// @endcond
 }
