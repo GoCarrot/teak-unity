fab1b032c50b49d24f400abb84f5a9bd712a8d4f
Pat Wilson <pat@teak.io>
2023-01-17 15:44:11 -0800

    ChannelStatus, OptOut state and UserData using these

---
+++ Assets/Teak/Teak.ChannelStatus.cs
@@ -0,0 +1,80 @@
+#region References
+/// @cond hide_from_doxygen
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+/// @endcond
+#endregion
+
+public partial class Teak {
+    /// <summary>
+    /// Encapsulation of the state of a Teak marketing channel.
+    /// </summary>
+    public class ChannelStatus {
+        /// <summary>Enum of possible state values for a Teak marketing channel.</summary>
+        public enum ChannelState : int {
+            OptOut = 0,
+            Available = 1,
+            OptIn = 2,
+            Absent = 3,
+            Unknown = 4
+        }
+
+        private static readonly ReadOnlyCollection<string> ChannelStatusName = new ReadOnlyCollection<string>(
+            new string[] {
+                "opt_out",
+                "available",
+                "opt_in",
+                "absent",
+                "unknown"
+            }
+        );
+
+        /// <summary>State of the marketing channel.</summary>
+        public ChannelState State {
+            get; private set;
+        }
+
+        /// <summary>The string version of the State of the marketing channel.</summary>
+        public string StateName {
+            get {
+                return ChannelStatusName[(int) this.State];
+            }
+        }
+
+        /// <summary>`true` if there was a failure the last time a delivery was attempted to this channel.</summary>
+        public bool DeliveryFault {
+            get; private set;
+        }
+
+        private void Assignment(int stateAsInt, bool deliveryFault) {
+            this.State = (ChannelState) stateAsInt;
+            this.DeliveryFault = deliveryFault;
+        }
+
+        /// <summary>Constructor</summary>
+        public ChannelStatus(ChannelState state, bool deliveryFault) {
+            int stateAsInt = (int) state;
+            if (stateAsInt < 0 || stateAsInt > 4) stateAsInt = 4;
+
+            Assignment(stateAsInt, deliveryFault);
+        }
+
+        /// <summary>Constructor</summary>
+        public ChannelStatus(Dictionary<string, object> json) {
+            int stateAsInt = 4; // Unknown
+            int.TryParse(json["state"] as string, out stateAsInt);
+
+            Assignment(stateAsInt, Convert.ToBoolean(json["delivery_fault"]));
+        }
+
+        /// <summary>Dictionary representation of this object, suitable for JSON encoding.</summary>
+        /// <returns>Dictionary representation of this object.</returns>
+        public Dictionary<string, object> ToDictionary() {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            dict.Add("state", this.StateName);
+            dict.Add("delivery_fault", this.DeliveryFault);
+            return dict;
+        }
+    }
+}
--- Assets/Teak/Teak.UserData.cs
+++ Assets/Teak/Teak.UserData.cs
@@ -13,20 +13,37 @@ public partial class Teak {
         /// <summary>Arbitrary, per-user, information sent from the Teak server.</summary>
         public Dictionary<string, object> AdditionalData { get; private set; }
 
-        /// <summary>True if the user has opted out of Teak email campaigns.</summary>
-        public bool OptOutEmail { get; private set; }
+        /// <summary></summary>
+        public ChannelStatus EmailStatus { get; private set; }
 
-        /// <summary>True if the user has opted out of Teak push notification campaigns.</summary>
-        public bool OptOutPush { get; private set; }
+        /// <summary></summary>
+        public ChannelStatus PushStatus { get; private set; }
+
+        /// <summary></summary>
+        public ChannelStatus SmsStatus { get; private set; }
 
         /// <summary>Push registration information for the current user, if available.</summary>
         public Dictionary<string, object> PushRegistration { get; private set; }
 
         internal UserData(Dictionary<string, object> json) {
             this.AdditionalData = json["additionalData"] as Dictionary<string, object>;
-            this.OptOutEmail = Convert.ToBoolean(json["optOutEmail"]);
-            this.OptOutPush = Convert.ToBoolean(json["optOutPush"]);
+            this.EmailStatus = new ChannelStatus(json["emailStatus"] as Dictionary<string, object>);
+            this.PushStatus = new ChannelStatus(json["pushStatus"] as Dictionary<string, object>);
+            this.SmsStatus = new ChannelStatus(json["smsStatus"] as Dictionary<string, object>);
             this.PushRegistration = json["pushRegistration"] as Dictionary<string, object>;
+        }
+
+
+        /// <summary>Dictionary representation of this object, suitable for JSON encoding.</summary>
+        /// <returns>Dictionary representation of this object.</returns>
+        public Dictionary<string, object> ToDictionary() {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            dict.Add("additionalData", this.AdditionalData);
+            dict.Add("emailStatus", this.EmailStatus.ToDictionary());
+            dict.Add("pushStatus", this.PushStatus.ToDictionary());
+            dict.Add("smsStatus", this.SmsStatus.ToDictionary());
+            dict.Add("pushRegistration", this.PushRegistration);
+            return dict;
         }
     }
 }
